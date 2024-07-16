af57d41a26b49bcc391ae90a7f1d1751488ce85b
Lukas Drobek <lukas.drobek1@gmail.com>
2024-07-16 22:05:45 +0200

    character functions

---
+++ libft.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldrobek <ldrobek@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 21:54:55 by ldrobek           #+#    #+#             */
+/*   Updated: 2024/07/16 22:03:27 by ldrobek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+// Characters functions
+int		ft_isalnum(char c);
+int		ft_isalpha(char c);
+int		ft_isascii(char c);
+int		ft_isdigit(char c);
+int		ft_isprint(char c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+// String functions
+
+// Memory functions
+
+// Numerical functions
+
+// File descriptor functions
+
+// List manipulation
+
+#endif
